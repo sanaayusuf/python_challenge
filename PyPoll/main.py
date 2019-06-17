d91a115477c5701e1a24f028677391eceecbb98f
Sana <sanakhan228@gmail.com>
2019-06-16 23:40:13 -0600

    1st try

---
+++ PyPoll/main.py
@@ -0,0 +1,17 @@
+import csv
+
+csvpath="./Resources/budget_data.csv"
+with open (csvpath,"r") as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    header=next(readCSV)
+    dates = []
+    profit_losses = []
+    for row in readCSV:
+        date = row[0]
+        profit_loss= row[1]
+        dates.append(date)
+        profit_losses.append(float(profit_loss))
+
+        
+    print(len(dates))
+    print(sum(profit_losses))
