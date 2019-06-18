791fc8156c4c62bd7329d95471fb611a6fc86548
Sana <sanakhan228@gmail.com>
2019-06-17 23:08:39 -0600

    Final Homework

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,58 @@
+import os
 import csv
 
-csvpath="./Resources/budget_data.csv"
-with open (csvpath,"r") as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    header=next(readCSV)
-    dates = []
-    profit_losses = []
-    for row in readCSV:
-        date = row[0]
-        profit_loss= row[1]
-        dates.append(date)
-        profit_losses.append(float(profit_loss))
+total_votes = 0
+candidate = ""
+candidate_votes = {}
+candidate_percentages ={}
+winner_votes = 0
+winner = ""
 
-        
-    print(len(dates))
-    print(sum(profit_losses))
+
+filepath = "./election_data.csv"
+with open(filepath,'r', newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    next(csvreader)
+
+    
+    for row in csvreader:
+        total_votes = total_votes + 1
+        candidate = row[2]
+        if candidate in candidate_votes:
+            candidate_votes[candidate] = candidate_votes[candidate] + 1
+        else:
+            candidate_votes[candidate] = 1
+
+
+for person, vote_count in candidate_votes.items():
+    candidate_percentages[person] = '{0:.0%}'.format(vote_count / total_votes)
+    if vote_count > winner_votes:
+        winner_votes = vote_count
+        winner = person
+
+
+
+
+print("Election Results")
+print("-------------------")
+print(f"Total Votes: {total_votes}")
+print("---------------------------")
+for person, vote_count in candidate_votes.items():
+    print(f"{person}: {candidate_percentages[person]} ({vote_count})")
+print(dashbreak)
+print(f"Winner: {winner}")
+print("------------------------")
+
+
+save_file = "results.txt"
+filepath = os.path.join(".", save_file)
+with open(filepath,'w') as text:
+    text.write(dashbreak + "\n")
+    text.write(f"Total Votes: {total_votes}" + "\n")
+    text.write(dashbreak + "\n")
+    for person, vote_count in candidate_votes.items():
+        text.write(f"{person}: {candidate_percentages[person]} ({vote_count})" + "\n")
+    text.write(dashbreak + "\n")
+    text.write(f"Winner: {winner}" + "\n")
+    text.write(dashbreak + "\n")
